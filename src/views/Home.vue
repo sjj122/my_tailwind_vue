287f503deef0e31b3f603d2e31a6307dc73b70b2
sjj122 <sjj737499>
2021-07-07 09:45:01 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="https://www.tailwindcss.cn/favicon-32x32.png">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  
+}) 
+</script>
